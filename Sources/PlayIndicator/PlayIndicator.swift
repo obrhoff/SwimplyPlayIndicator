dc52f3b2efa27975127804f6dfcabf667e3d3a4d
Dennis Oberhoff <dennis@obrhoff.de>
2020-02-01 13:46:21 +0100

    Added random values
    - Some code cleanup
    - fixed a animation hanging bug
    - added rando animation values / timings

--- Sources/PlayIndicator/PlayIndicator.swift
+++ Sources/PlayIndicator/PlayIndicator.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 public struct PlayIndicator: View {
-    private struct AnimationValues: Identifiable {
+    struct AnimationValue: Identifiable {
         let id: Int
         let maxValue: CGFloat
         let animation: Animation
@@ -15,58 +15,71 @@ public struct PlayIndicator: View {
 
     @Binding private var state: AudioState
     @State private var animating: Bool = false
-    private let lineColor: Color
     private let minimalValue: CGFloat = 0.1
-    private let values: [AnimationValues]
+    public let lineColor: Color
+    public let lineCount: Int
 
     private var stopAnimation: Animation {
-        Animation.easeOut.delay(0.05)
+        Animation.easeOut(duration: 0.2)
     }
 
-    public init(state: Binding<AudioState>, lineColor: Color = Color.black) {
+    private var opacity: Double {
+        state == .stop ? 0.0 : 1.0
+    }
+
+    private var opacityAnimation: Animation? {
+        Animation.linear
+    }
+
+    private var animationValues: [AnimationValue] {
+        let valueRange: ClosedRange<CGFloat> = (0.2 ... 1.0)
+        let speedRange: ClosedRange<Double> = (0.7 ... 1.2)
+        let animations: [Animation] = [.easeIn, .easeOut, .easeInOut, .linear]
+        let values = (0 ..< lineCount).compactMap { (id) -> AnimationValue? in
+            guard let animation = animations.randomElement() else { return nil }
+            return AnimationValue(id: id, maxValue: CGFloat.random(in: valueRange),
+                                  animation: animation.speed(Double.random(in: speedRange)))
+        }
+        return values
+    }
+
+    public init(state: Binding<AudioState>, lineCount: Int = 4, lineColor: Color = Color.black) {
         self._state = state
+        self.lineCount = lineCount
         self.lineColor = lineColor
-        self.values = [AnimationValues(id: 1, maxValue: 0.4, animation: Animation.easeIn(duration: 0.175)),
-                       AnimationValues(id: 2, maxValue: 0.9, animation: Animation.easeInOut(duration: 0.2)),
-                       AnimationValues(id: 3, maxValue: 0.6, animation: Animation.easeOut(duration: 0.175)),
-                       AnimationValues(id: 4, maxValue: 0.4, animation: Animation.easeInOut(duration: 0.285))]
     }
 
     public var body: some View {
         GeometryReader { reader in
             HStack(alignment: .center, spacing: 1) {
-                ForEach(self.values) { value in
-                    PlaybackBarView(maxValue:
-                        self.animating ? (self.state == .play ? value.maxValue : self.minimalValue) : self.minimalValue)
+                ForEach(self.animationValues) { value in
+                    LineView(maxValue:
+                        self.state == .play ? value.maxValue : self.minimalValue)
                         .stroke(self.lineColor, lineWidth: reader.size.width / 8)
-                        .animation(self.state == .play ? value.animation.repeatForever() : self.stopAnimation)
+                        .animation(self.state == .play ? value.animation.repeatForever() : Animation.easeOut(duration: 0.3))
                 }
-            }.drawingGroup()
-                .opacity(self.state == .stop ? 0.0 : 1.0)
-                .animation(Animation.linear)
-                .onAppear {
-                    self.animating.toggle()
-                }
-        }
+            }
+
+        }.opacity(self.opacity)
+            .drawingGroup()
+            .animation(Animation.linear)
     }
 }
 
-private struct PlaybackBarView: Shape {
-    var maxValue: CGFloat
+private extension PlayIndicator {
+    struct LineView: Shape {
+        var maxValue: CGFloat
 
-    var animatableData: CGFloat {
-        get {
-            return maxValue
+        var animatableData: CGFloat {
+            get { maxValue }
+            set { maxValue = newValue }
         }
-        set {
-            maxValue = newValue
-        }
-    }
 
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: .init(x: rect.midX, y: rect.maxY))
-        path.addLine(to: .init(x: rect.midX, y: rect.maxY - (maxValue * rect.height)))
-        return path
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            path.move(to: .init(x: rect.midX, y: rect.maxY))
+            path.addLine(to: .init(x: rect.midX, y: rect.maxY - (maxValue * rect.height)))
+            return path
+        }
     }
 }
